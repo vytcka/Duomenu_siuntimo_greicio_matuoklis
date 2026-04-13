e4f0d01ff987c26aa076e5446c0f3f1af9fc61b5
root <root@MSI>
2026-04-13 16:21:19 -0700

    Toliau vystomą užklaūsų implomentacija

--- src/process.c
+++ src/process.c
@@ -1,25 +1,42 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 
-	void intialiseCurl(){
+	void initialiseCurl(){
 		curl_global_init(CURL_GLOBAL_ALL);
+	}
+	int cleanHandle(CURL* handle){
+		if (hadle == NULL){
+			return 1;
+		}
+		curl_easy_cleanup(handle);
+		return 0;
 	}
 
 	CURL* getHandle(){
-	return curl_easy_init();
+		return curl_easy_init();
 	}
 
 	
 	int makeRequest(CURL* handle, char* url){
-	if (handle == NULL){
-	return 1;
-	}
-	curl_easy_setopt(handle, CURLOPT_URL, url);
-	curl_easy_setopt(handle, CURLOPT_TIMEOUT, 5L);
-	curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L);
-	curl_easy_perform(handle);
-	curl_easy_cleanup(handle);
-	return 0;
+		if (handle == NULL){
+			return 1;
+		}
+		//žiūrimas rezultatas requesto
+		Curlcode rezultatas;
+		
+		//configuruojamas requestas, kad po 5 sekundžų timeoutas ir, kad persekiotu redirectionus
+		curl_easy_setopt(handle, CURLOPT_URL, url);
+		curl_easy_setopt(handle, CURLOPT_TIMEOUT, 5L);
+		curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L);
+		
+		//vykdomas requestas
+		rezultatas = curl_easy_perform(handle);
+		if (rezultatas != 0){
+			cleanHandle(handle);
+			return 1;
+		}
+		cleanHandle(handle);
+		return 0;
 	}
 
 
