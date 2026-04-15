51dd42389661e336ab0a2699da84d20fdfce766b
root <root@MSI>
2026-04-15 12:10:33 -0700

    toliau vystomas projektas

--- src/main.c
+++ src/main.c
@@ -1,10 +1,32 @@
-#include "processInfo.h"
-#include <
+#include "requestInfo.h"
+#include "parser.h"
+#include "process.h"
 #include <stdlib.h>
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
+#include <stdio.h>
 
 
-int main(){
+void downloadTest(CURL* handle){
+
+}
+
+
+int main(int argc, char** argv){
+	//  -a - automatizuotas testas, viskas integruota, lokacija programos naudojimo
+	//  -d - download testas
+	//  -u - upload test
+	//  -s find best server.
+	//  -l location
+	int i;
+	printf("argc = %d\n", argc);
+
+	for (i = 0; i<argc; i++){
+		puts(argv[i]);
+	}
+	
+	
+
 	return 1;
+
 }
